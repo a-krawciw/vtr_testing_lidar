05540072179444b61e8e6d903fb7f3fd113907cb
Yuchen Wu <cheneyuwu@gmail.com>
2022-05-22 15:41:24 -0400

    scripts for map maintenance experiments

---
+++ src/vtr_testing_honeycomb/script/map_maintenance/commands.sh
@@ -0,0 +1,47 @@
+echo "This script contains instructions to run all tests, do not run this script directly."
+exit 1
+
+## First launch RViz for visualization
+source /opt/ros/galactic/setup.bash                   # source the ROS environment
+ros2 run rviz2 rviz2 -d ${VTRSRC}/rviz/honeycomb.rviz # launch rviz
+
+## Then in another terminal, launch rqt_reconfigure for control
+## current supported dynamic reconfigure parameters: control_test.play and controL_test.delay_millisec
+source /opt/ros/galactic/setup.bash
+ros2 run rqt_reconfigure rqt_reconfigure
+
+############################################################
+#### Now start another terminal and run testing scripts ####
+
+## Terminal Setup (Run Following Once)
+
+## Define the following environment variables VTRH=VTR Honeycomb
+export VTRHROOT=/home/yuchen/ASRL/vtr_testing_lidar
+export VTRHDATA=${VTRDATA}/utias_parkinglot
+export VTRHRESULT=${VTRTEMP}/testing/utias_parkinglot
+mkdir -p ${VTRHRESULT}
+
+# Source the VTR environment with the testing package
+source ${VTRHROOT}/install/setup.bash
+
+# Choose a Teach (ODO_INPUT) and Repeat (LOC_INPUT) run from boreas dataset
+ODO_INPUT=rosbag2_2021_08_23-09_25_37
+NUM_FRAMES=1700
+
+LOC_INPUT=rosbag2_2021_08_23-09_57_54
+
+## Some use flags
+##   --prefix 'gdb -ex run --args'
+
+## Using ONE of the following commands to launch a test
+# (TEST 1) Perform data preprocessing on a sequence (e.g. keypoint extraction)
+bash ${VTRHROOT}/src/vtr_testing_honeycomb/script/test_preprocessing.sh ${ODO_INPUT}
+# (TEST 2) Perform odometry on a sequence
+bash ${VTRHROOT}/src/vtr_testing_honeycomb/script/map_maintenance/odometry.sh ${ODO_INPUT} ${NUM_FRAMES}
+# (TEST 3) Perform localization on a sequence (only run this after TEST 2)
+bash ${VTRHROOT}/src/vtr_testing_honeycomb/script/test_localization.sh ${ODO_INPUT} ${LOC_INPUT}
+
+# (TEST 4) Intra Exp Merging
+RUN_ID=0
+bash ${VTRHROOT}/src/vtr_testing_honeycomb/script/map_maintenance/intra_exp_merging.sh ${RUN_ID}
+bash ${VTRHROOT}/src/vtr_testing_honeycomb/script/map_maintenance/dynamic_detection.sh ${RUN_ID}
---
+++ src/vtr_testing_honeycomb/script/map_maintenance/dynamic_detection.sh
@@ -0,0 +1,11 @@
+# Get arguments
+RUN_ID=$1
+
+# Source the VTR environment with the testing package
+source ${VTRHROOT}/install/setup.bash
+
+ros2 run vtr_testing_honeycomb vtr_testing_honeycomb_dynamic_detection \
+  --ros-args -r __ns:=/vtr \
+  --params-file ${VTRHROOT}/src/vtr_testing_honeycomb/config/honeycomb.yaml \
+  -p data_dir:=${VTRHRESULT}/main \
+  -p run_id:=${RUN_ID}
